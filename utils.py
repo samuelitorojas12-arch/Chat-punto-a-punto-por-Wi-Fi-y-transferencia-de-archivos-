c6d0a9aba17524a3bdae53a22ef324a9454568bd
yoyo3056 <ebelloyoval@gmail.com>
2026-04-24 22:59:54 -0600

    Implement SHA256 hash calculation function
    Add function to calculate SHA256 hash of a file

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
+import hashlib
 
+def calculate_sha256(file_path):
+sha256 = hashlib.sha256()
+with open(file_path, "rb") as f:
+for block in iter(lambda: f.read(4096), b""):
+sha256.update(block)
+return sha256.hexdigest()
+Hecho por: Elsy
