841133c38d660cf0b324cbda4d32f7a8cc8254bd
yoyo3056 <ebelloyoval@gmail.com>
2026-04-24 23:21:51 -0600

    Implement basic client chat functionality

--- client_chat.py
+++ client_chat.py
@@ -1,1 +1,26 @@
+import socket
+import threading
 
+IP_DESTINO = 'IP_DEL_SERVIDOR'
+PUERTO_DESTINO = 5000
+
+sock_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_cliente.connect((IP_DESTINO, PUERTO_DESTINO))
+
+def escuchar_mensajes():
+    while True:
+        try:
+            datos = sock_cliente.recv(1024).decode('utf-8')
+            print("\nMensaje recibido:", datos)
+        except:
+            print("Conexión perdida")
+            sock_cliente.close()
+            break
+
+def enviar_mensajes():
+    while True:
+        texto = input()
+        sock_cliente.send(texto.encode('utf-8'))
+
+threading.Thread(target=escuchar_mensajes).start()
+threading.Thread(target=enviar_mensajes).start()
