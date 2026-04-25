c42b198320b583d9c78ec028fad87d522897345c
dantemmanuels7-wq <dantemmanuels7@gmail.com>
2026-04-25 00:09:55 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,3 +7,25 @@ for block in iter(lambda: f.read(4096), b""):
 sha256.update(block)
 return sha256.hexdigest()
 Hecho por: Elsy
+
+"version corregida y mejorada por Dante
+
+import hashlib
+
+def calcular_sha256(ruta_archivo):
+    sha256 = hashlib.sha256()
+
+    try:
+        with open(ruta_archivo, "rb") as f:
+            for bloque in iter(lambda: f.read(4096), b""):
+                sha256.update(bloque)
+
+        return sha256.hexdigest()
+
+    except FileNotFoundError:
+        print("Error: archivo no encontrado")
+        return None
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
